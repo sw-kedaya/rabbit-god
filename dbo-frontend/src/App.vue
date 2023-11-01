4d83df1a31b10b0c6623e01fdcf9ac0e8b2fb096
achun <swwcc1103@163.com>
2023-11-01 11:13:25 +0800

    增加页面部分文字可选中效果。

--- dbo-frontend/src/App.vue
+++ dbo-frontend/src/App.vue
@@ -5,7 +5,7 @@ const local = zhCN
 </script>
 
 <template>
-  <div class="container">
+  <div class="container user-selectable">
     <el-config-provider :locale="local">
       <router-view/>
     </el-config-provider>
@@ -18,5 +18,12 @@ const local = zhCN
   min-width: 512px;
   background-image: url("@/assets/images/bg.png"); /* 设置背景图片的 URL */
   background-repeat: repeat; /* 将背景图片设置为平铺 */
+}
+
+.user-selectable {
+  -webkit-user-select: text; /* Chrome, Safari, Opera */
+  -moz-user-select: text; /* Firefox */
+  -ms-user-select: text; /* Internet Explorer/Edge */
+  user-select: text; /* 非前缀版本，目前被大多数浏览器支持 */
 }
 </style>
